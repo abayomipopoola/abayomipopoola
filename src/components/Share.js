2f155ac64778577d4def5c6847729e6a452d4d62
Abayomi Popoola <abayomipopoola@gmail.com>
2020-05-07 04:19:55 +0100

    fix buttons

--- src/components/Share.js
+++ src/components/Share.js
@@ -14,41 +14,23 @@ import {
   faWhatsapp,
 } from '@fortawesome/free-brands-svg-icons';
 
+function isMobileOrTablet() {
+  return /(android|iphone|ipad|mobile)/i.test(navigator.userAgent);
+}
+
 library.add(faFacebookF, faLinkedinIn, faTwitter, faWhatsapp);
 
 const Share = ({ socialConfig }) => (
-  <div className="share">
-    <ul className="list-inline list-reset">
-      <li>Share</li>
-      <li>
-        <TwitterShareButton resetButtonStyle={false} url={socialConfig.config.url} className="btn share-tw" title={socialConfig.config.title} via={socialConfig.twitterHandle}>
-          <span className="icon">
-            <FontAwesomeIcon icon={['fab', 'twitter']}/>
-          </span>
-        </TwitterShareButton>
-      </li>
-      <li>
-        <FacebookShareButton resetButtonStyle={false} url={socialConfig.config.url} className="btn share-fb">
-          <span className="icon">
-            <FontAwesomeIcon icon={['fab', 'facebook-f']}/>
-          </span>
-        </FacebookShareButton>
-      </li>
-      <li>
-        <LinkedinShareButton resetButtonStyle={false} url={socialConfig.config.url} className="btn share-in" title={socialConfig.config.title}>
-          <span className="icon">
-            <FontAwesomeIcon icon={['fab', 'linkedin-in']}/>
-          </span>
-        </LinkedinShareButton>
-      </li>
-      <li>
-        <WhatsappShareButton resetButtonStyle={false} url={socialConfig.config.url} className="btn share-wh" title={socialConfig.config.title}>
-          <span className="icon">
-            <FontAwesomeIcon icon={['fab', 'whatsapp']}/>
-          </span>
-        </WhatsappShareButton>
-      </li>
-    </ul>
+  <div className="share reset-inline">
+    <span className="text">Share</span>
+    <a className="twitter" href={`https://twitter.com/intent/tweet?text=${socialConfig.config.title}&url=${socialConfig.config.url}&via=${socialConfig.twitterHandle}`}
+       target="blank"><FontAwesomeIcon icon={['fab', 'twitter']}/></a>
+    <a className="facebook" href={`https://www.facebook.com/sharer/sharer.php?u=${socialConfig.config.url}`}
+       target="blank"><FontAwesomeIcon icon={['fab', 'facebook-f']}/></a>
+    <a className="linkedin" href={`https://www.linkedin.com/shareArticle?mini=true&url=${socialConfig.config.url}&title=${socialConfig.config.title}`}
+       target="blank"><FontAwesomeIcon icon={['fab', 'linkedin-in']}/></a>
+    <a className="whatsapp" href={`https://${(isMobileOrTablet() ? 'api' : 'web')}.whatsapp.com/send?text=${socialConfig.config.title} ${socialConfig.config.url}`}
+       target="blank"><FontAwesomeIcon icon={['fab', 'whatsapp']}/></a>
   </div>
 );
 
