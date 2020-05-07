29d8c9e10490fc661d3b7f4165d22053629eabad
Abayomi Popoola <abayomipopoola@gmail.com>
2020-05-07 04:21:15 +0100

    fix bug

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  FacebookShareButton,
-  LinkedinShareButton,
-  TwitterShareButton,
-  WhatsappShareButton,
-} from 'react-share';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faFacebookF,
