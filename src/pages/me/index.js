74d38c3a5ca71a67d0b5bde5d7baae3e02b96758
Abayomi Popoola <abayomipopoola@gmail.com>
2021-12-08 22:36:11 +0100

    Update about me

--- src/pages/me/index.js
+++ src/pages/me/index.js
@@ -26,7 +26,7 @@ class PortfolioPage extends React.Component {
       and ensure that projects are completed to specification, within an established time frame and budget.
         </p>
         <p>
-        I’m currently the VP, Engineering at  <a href="https://www.dotpay.africa/" target="_blank" >DotPay</a>, 
+        I’m currently a VP of Engineering at  <a href="https://www.dotpay.africa/" target="_blank" >DotPay</a>, 
         a startup that's democratising financial services with digital banking tools for underserved consumers in emerging markets. 
         I used to freelance at Bytecreatives.
         </p>
